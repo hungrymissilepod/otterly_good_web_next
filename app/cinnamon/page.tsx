96c340579884191a582e32c0b4d41f90251b4555
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2024-01-07 22:41:17 +0000

    add carousel

--- app/cinnamon/page.tsx
+++ app/cinnamon/page.tsx
@@ -1,17 +1,132 @@
-import React from "react";
+'use client';
+import React, { useRef, useState } from "react";
+import Image from 'next/image';
+import { CiLocationOn } from "react-icons/ci";
+
 import HeroSectionCinnamon from '../components/cinnamon/hero-section';
 import CaseStudySectionCinnamon from '../components/cinnamon/case-study-section';
 import ServicesSectionCinnamon from "../components/cinnamon/services-section";
 import FooterSectionCinnamon from "../components/cinnamon/footer-section";
 import { NavBarCinnamon } from "../components/cinnamon/navbar-cinnamon";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { clsx } from 'clsx';
 
-// TODO: add mobile nav bar
+
 // TODO: try out css tilt effect on an image when hovering over it
 // TODO: try out more interactive elements like carousel
+
+// TODO: check errors in console
+// TODO: check lighthouse score. How do we improve it?
+// TODO: check SEO score. How do we improve it?
 export default function Page() {
+  const blockRef = useRef(null);
+  var [index, setIndex] = useState(0);
+  const [pos, setPos] = useState(0);
+  const x = 0;
+  const y = 0;
+  var p = index * 500;
+  var styles = {
+    transform: `translate(${p}px, ${y}px)`
+  };
+
+  const next = () => {
+    if (index >= 0) {
+      return;
+    }
+    index = index + 1;
+    setIndex(index);
+    p = (index * 500);
+    styles = {
+      transform: `translate(${p}px, ${y}px)`
+    }
+    console.log(`update x: ${p}`);
+    console.log(`next index: ${index}`);
+  }
+
+  const previous = () => {
+    if (index <= -2) return;
+    setIndex(index - 1);
+    p = index * 500;
+    styles = {
+      transform: `translate(${p}px, ${y}px)`
+    }
+    console.log(`update x: ${p}`);
+    console.log(`index: ${index}`);
+  }
+
   return (
     <body>
       <NavBarCinnamon />
+      <div className='mx-auto my-6 lg:my-20 max-w-6xl px-4 xl:px-0'>
+        <div className="flex justify-center w-full">
+          <div className="mx-auto max-w-6xl">
+            <h2 className="text-black text-5xl font-bold pb-12">Our offices</h2>
+            <div className="flex flex-row gap-10 overflow-x-visible ease-in-out duration-300" style={styles}>
+              <div className="block" style={{ "minWidth": "500px" }}>
+                <Image
+                  src="https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="London Office"
+                  width={500}
+                  height={500}
+                />
+                <div className="flex flex-row items-center py-6">
+                  <CiLocationOn style={{ "fontSize": "20px", "color": "black", "strokeWidth": "1px" }} />
+                  <div className="px-3">
+                    <h1 className="text-black font-bold text-xl">London</h1>
+                    <h1 className="text-black">21b Baker Street, London, United Kingdom</h1>
+                  </div>
+                </div>
+              </div>
+
+              <div className="block" style={{ "minWidth": "500px" }}>
+                <Image
+                  src="https://images.unsplash.com/photo-1485871981521-5b1fd3805eee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="New York Office"
+                  width={500}
+                  height={500}
+                />
+                <div className="flex flex-row items-center py-6">
+                  <CiLocationOn style={{ "fontSize": "20px", "color": "black", "strokeWidth": "1px" }} />
+                  <div className="px-3">
+                    <h1 className="text-black font-bold text-xl">New York</h1>
+                    <h1 className="text-black">7th Ave, New York, United States</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="block" style={{ "minWidth": "500px" }}>
+                <Image
+                  src="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Tokyo Office"
+                  width={500}
+                  height={500}
+                />
+                <div className="flex flex-row items-center py-6">
+                  <CiLocationOn style={{ "fontSize": "20px", "color": "black", "strokeWidth": "1px" }} />
+                  <div className="px-3">
+                    <h1 className="text-black font-bold text-xl">Tokyo</h1>
+                    <h1 className="text-black">99 Kichijojo Sun Road, Tokyo, Japan</h1>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <ul className="flex-row flex gap-x-10">
+          <li className={`relative top-0 ease-in duration-100 drop-shadow-md ${index < 0 ? 'text-indigo-500 hover:-top-2' : 'text-gray-400'} `}>
+            <button>
+              <FaArrowLeft size={40} onClick={() => next()} style={{ "fontSize": "20px", "strokeWidth": "1px" }} />
+            </button>
+          </li>
+          <li className={`relative top-0 ease-in duration-100 drop-shadow-md ${index > -2 ? 'text-indigo-500 hover:-top-2' : 'text-gray-400'}`}>
+            <button>
+              <FaArrowRight size={40} onClick={() => previous()} style={{ "fontSize": "20px", "strokeWidth": "1px" }} />
+            </button>
+          </li>
+        </ul>
+
+      </div>
       <HeroSectionCinnamon />
       <CaseStudySectionCinnamon />
       <ServicesSectionCinnamon />
--- app/components/cinnamon/navbar-cinnamon.tsx
+++ app/components/cinnamon/navbar-cinnamon.tsx
@@ -1,41 +1,59 @@
 'use client'
 import Image from 'next/image';
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@nextui-org/button";
 import Link from 'next/link';
-import { CiMenuBurger } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
 
 function NavBarCinnamon() {
+  const [isClick, setisClick] = useState(false);
 
-  const onClick = (e: any) => {
-    console.log('click');
-    // setDarkMode(e.target.value ? "light" : "dark");
-  };
+  const toggleNavBar = () => {
+    setisClick(!isClick);
+  }
+
   return (
-    <nav className='flex justify-between items-center mx-auto max-w-6xl px-4 xl:px-0 py-6 bg-white z-50 sticky top-0'>
-      <Image
-        className=' inline-block dark:drop-shadow-[0_0_0.3rem_#ffffff70] z-50'
-        src='/next.svg'
-        alt='Next.js Logo'
-        width={180}
-        height={37}
-        priority
-      />
-      <ul className="">
-        <NavBarLink title='Projects' href='/projects' />
-        <NavBarLink title='Services' href='/services' />
-        <NavBarLink title='About Us' href='/about' />
-        <NavBarLink title='Blog' href='/blog' />
-        <div className='list-none inline-block px-2'></div>
-        <Link href="/">
-          <Button className='hidden lg:inline-block bg-indigo-500 px-5 py-2' color="primary" >
-            Let&#39;s collaborate
-          </Button>
-        </Link>
-        <Link className='align-middle inline-block lg:hidden' href={''} onClick={onClick}>
-          <CiMenuBurger style={{ "fontSize": "30px", "color": "black" }} />
-        </Link>
-      </ul>
+    <nav className='mx-auto max-w-6xl px-4 xl:px-0 py-6 bg-white z-50 sticky top-0'>
+      <div className='flex justify-between items-center'>
+        <Image
+          className=' inline-block dark:drop-shadow-[0_0_0.3rem_#ffffff70] z-50'
+          src='/next.svg'
+          alt='Next.js Logo'
+          width={180}
+          height={37}
+          priority
+        />
+        <ul className="">
+          <NavBarLink title='Projects' href='/projects' />
+          <NavBarLink title='Services' href='/services' />
+          <NavBarLink title='About Us' href='/about' />
+          <NavBarLink title='Blog' href='/blog' />
+          <div className='list-none inline-block px-2'></div>
+          <Link href="/">
+            <Button className='hidden lg:inline-block bg-indigo-500 px-5 py-2' color="primary" >
+              Let&#39;s collaborate
+            </Button>
+          </Link>
+          <Link className='align-middle inline-block lg:hidden' href={''} onClick={toggleNavBar}>
+            {
+              !isClick && <HiOutlineMenuAlt1 style={{ "fontSize": "30px", "color": "black" }} />
+            }
+            {
+              isClick && <IoCloseOutline style={{ "fontSize": "30px", "color": "black" }} />
+            }
+          </Link>
+        </ul>
+      </div>
+      {
+        isClick &&
+        <div className='md:hidden w-full text-center  items-center'>
+          <a className="block py-6 text-black font-medium hover:text-indigo-500" href="/projects">Projects</a>
+          <a className="block py-6 text-black font-medium hover:text-indigo-500" href="/services">Services</a>
+          <a className="block py-6 text-black font-medium hover:text-indigo-500" href="/about">About Us</a>
+          <a className="block py-6 text-black font-medium hover:text-indigo-500" href="/blog">Blog</a>
+        </div>
+      }
     </nav>
   );
 }
