d1e0d2aa759b2b12616e707760360b19b77d9dbb
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2024-01-16 20:38:09 +0000

    chore: add todos

--- app/cinnamon/page.tsx
+++ app/cinnamon/page.tsx
@@ -6,6 +6,9 @@ import { NavBarCinnamon } from "../components/cinnamon/navbar-cinnamon";
 import OfficesCarouselCinnamon from "../components/cinnamon/offices-carousel";
 
 // TODO: try out css tilt effect on an image when hovering over it
+// TODO: try out cursor dot animation (hyperplexed or that indian guy)
+// TODO: try changing out cursor for an image or shape
+// TODO: try out that card page sticky scrolling
 // TODO: check errors in console
 export default function Page() {
   return (
