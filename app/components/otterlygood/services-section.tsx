36f739be3f6223e1f16f299816e19447231f8eb3
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2024-01-26 23:05:18 +0000

    feat: Added GSAP

--- app/components/otterlygood/services-section.tsx
+++ app/components/otterlygood/services-section.tsx
@@ -1,3 +1,10 @@
+'use client';
+import { gsap } from 'gsap';
+import { useLayoutEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+
+
+
 function ServicesSection() {
   return (
     <div className="my-20 py-20 mx-auto">
@@ -20,9 +27,15 @@ function ServicesSection() {
 }
 
 function ServicesTile({ title, desc }: { title: string, desc: string }) {
+  const box = useRef(null);
+
+  useGSAP((context) => {
+    gsap.to(".box", { rotation: "+=360" });
+  }, { scope: box });
+
   return (
-    <div className="hover:bg-og-orange">
-      <div className="group flex flex-row justify-between items-center max-w-7xl mx-auto">
+    <div className="bg-og-orange hover:bg-transparent" ref={box}>
+      <div className="box group flex flex-row justify-between items-center max-w-7xl mx-auto">
         <h2 className="text-og-black text-8xl font-black group-hover:text-og-white">{title}</h2>
         <p className="hidden group-hover:inline text-white">{desc}</p>
       </div>
